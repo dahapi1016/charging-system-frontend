2ac9a2272fa3503877c84ff8b7c550a2ba3c356e
Diaozhiyang <shenwu031016@126.com>
2025-06-08 18:21:06 +0800

    加了token

--- src/net/index.js
+++ src/net/index.js
@@ -4,22 +4,21 @@ import { ElMessage } from "element-plus";
 const defaultError = () => ElMessage.error('发生了一些错误，请联系管理员')
 const defaultFailure = (code, message) => ElMessage.warning(code + ':' + message)
 
-function post(api, data, success, failure = defaultFailure, error = defaultError) {  // 这里data是前端向后端发送的请求内容
+function internalPost(api, data, headers, success, failure = defaultFailure, error = defaultError) {  // 这里data是前端向后端发送的请求内容
     axios.post(api, data, {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
+        headers: headers,
         withCredentials: true
     }).then(({data}) => {  // 这里data的属性对应于后端向前端返回的Result<T>的属性，有code(网络状态码)，message(具体信息)，data(具体数据)
         if(data.code == 200)
-            success(data.message)
+            success(data.data, data.message)
         else
             failure(data.code, data.message)
     }).catch(error)
 }
 
-function get(api, success, failure = defaultFailure, error = defaultError) {
+function internalGet(api, headers, success, failure = defaultFailure, error = defaultError) {
     axios.get(api, {
+        headers: headers,
         withCredentials: true
     }).then(({data}) => {
         if(data.code == 200)
@@ -29,4 +28,19 @@ function get(api, success, failure = defaultFailure, error = defaultError) {
     }).catch(error)
 }
 
-export { get, post }
+function post(api, data, success, failure = defaultFailure, error = defaultError) {
+    internalPost(api, data, {'Authorization': `Bearer ${localStorage.getItem('token')}`}, success, failure, error)
+}
+
+function get(api, success, failure = defaultFailure, error = defaultError) {
+    internalGet(api, {'Authorization': `Bearer ${localStorage.getItem('token')}`}, success, failure, error)
+}
+
+function login(data) {
+    internalPost('/api/user/login', data, {'Content-Type': 'application/x-www-form-urlencoded'}, (data, message) => {
+        ElMessage.success(message);
+        localStorage.setItem('token', data.token);  // token存到localStorage
+    })
+}
+
+export { get, post, login }
--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -1,24 +1,21 @@
 <script>
-import { ElMessage } from "element-plus";
-import { post } from "@/net";
+import { login } from "@/net";
 
 export default {
   data() {
     return {
-      adminname: '',
+      username: '',
       password: ''
     };
   },
   methods: {
-    login() {
+    adminLogin() {
       const data = {
-        adminname: this.adminname,
+        username: this.username,
         password: this.password
       };
-      post('/admin/login', data, (message) => {
-        ElMessage.success(message);
-        this.$router.push('/admin/index');
-      });
+      login(data)
+      this.$router.push('/admin/index');
     }
   }
 };
@@ -29,14 +26,14 @@ export default {
     <el-card class="login-card">
       <div class="card-content">
         <h2 class="text-center">管理员登录</h2>
-        <el-form @submit.prevent="login" class="login-form">
+        <el-form @submit.prevent="adminLogin" class="login-form">
           <el-form-item label="管理员名:">
-            <el-input v-model="adminname" placeholder="请输入管理员名" />
+            <el-input v-model="username" placeholder="请输入管理员名" />
           </el-form-item>
           <el-form-item label="密码:">
             <el-input type="password" v-model="password" placeholder="请输入密码" />
           </el-form-item>
-          <el-button type="primary" class="login-button" @click="login">登录</el-button>
+          <el-button type="primary" class="login-button" @click="adminLogin">登录</el-button>
         </el-form>
         <p class="text-center">
           <router-link to="/user/login">普通用户登录</router-link>
